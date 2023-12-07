ba8a33695df5636effc7dcc55412254867807c58
Chirag Gupta <chirag.gupta2992@gmail.com>
2023-12-07 17:31:42 -0500

    Added some helper methods in the container

--- src/schedules/feedbackScheduling/FeedbackTasksContainer.java
+++ src/schedules/feedbackScheduling/FeedbackTasksContainer.java
@@ -2,32 +2,81 @@ package schedules.feedbackScheduling;
 
 import tasks.Task;
 
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
 
-public class FeedbackTasksContainer implements Iterable<Task> {
+public class FeedbackTasksContainer implements Iterable<BlockingQueue<Task>> {
 
-    @Override
-    public Iterator<Task> iterator() {
-        return new FeedbackIterator(this);
+    private final BlockingQueue<Task> firstPriorityQueue = new LinkedBlockingQueue<>();
+    private final BlockingQueue<Task> secondPriorityQueue = new LinkedBlockingQueue<>();
+    private final BlockingQueue<Task> thirdPriorityQueue = new LinkedBlockingQueue<>();
+    private final BlockingQueue<Task> fourthPriorityQueue = new LinkedBlockingQueue<>();
+
+    private final FeedbackIterator iterator = new FeedbackIterator(this);
+
+    public void promote() throws InterruptedException {
+        BlockingQueue<Task> currentQueue = iterator.currentQueue();
+        if (!currentQueue.equals(firstPriorityQueue)) {
+            Task task = currentQueue.take();
+            BlockingQueue<Task> higherPriorityQueue = iterator.peekPreviousQueue();
+            higherPriorityQueue.put(task);
+        }
     }
 
-    class FeedbackIterator implements Iterator<Task> {
-        private final FeedbackTasksContainer taskContainer;
+    public void demote() throws InterruptedException {
+        BlockingQueue<Task> currentQueue = iterator.currentQueue();
+        if (!currentQueue.equals(fourthPriorityQueue)) {
+            Task task = currentQueue.take();
+            BlockingQueue<Task> lowerPriorityQueue = iterator.peekPreviousQueue();
+            lowerPriorityQueue.put(task);
+        }
+    }
+
+    @Override
+    public Iterator<BlockingQueue<Task>> iterator() {
+        return iterator;
+    }
+
+     class FeedbackIterator implements Iterator<BlockingQueue<Task>> {
+        private final ArrayList<BlockingQueue<Task>> listOfQueues = new ArrayList<>(4);
+        private int currentQueueIndex = -1;
 
         public FeedbackIterator(FeedbackTasksContainer taskContainer) {
-            this.taskContainer = taskContainer;
+            listOfQueues.add(taskContainer.firstPriorityQueue);
+            listOfQueues.add(taskContainer.secondPriorityQueue);
+            listOfQueues.add(taskContainer.thirdPriorityQueue);
+            listOfQueues.add(taskContainer.fourthPriorityQueue);
         }
 
         @Override
         public boolean hasNext() {
-            return false;
+            return true;
         }
 
         @Override
-        public Task next() {
-            return null;
+        public BlockingQueue<Task> next() {
+            if (currentQueueIndex == listOfQueues.size() - 1) {
+                currentQueueIndex = 0;
+            } else {
+                currentQueueIndex++;
+            }
+            return listOfQueues.get(currentQueueIndex);
+        }
+
+        public BlockingQueue<Task> currentQueue() {
+            return listOfQueues.get(currentQueueIndex);
+        }
+
+        public BlockingQueue<Task> peekNextQueue() {
+            int peekIndex = currentQueueIndex == listOfQueues.size() - 1 ? 0 : currentQueueIndex + 1;
+            return listOfQueues.get(peekIndex);
+        }
+
+        public BlockingQueue<Task> peekPreviousQueue() {
+            int peekIndex = currentQueueIndex > 0 ? currentQueueIndex - 1 : listOfQueues.size() - 1;
+            return listOfQueues.get(peekIndex);
         }
     }
 }
