7292f2128cb7fa8811520a7d51c3a946dc98f452
Pranit2411 <pranitmenkar24@gmail.com>
2023-12-07 23:36:54 -0500

    created a linkedblocking queue along with helperfunctions

--- src/schedules/fcfsScheduling/FCFSScheduler.java
+++ src/schedules/fcfsScheduling/FCFSScheduler.java
@@ -1,9 +1,17 @@
 package schedules.fcfsScheduling;
 
 
+import java.util.concurrent.BlockingQueue;
+
 import runner.Runner;
 import tasks.Task;
 
 public class FCFSScheduler {
+    private final FCFSTasksContainer fcfsTasksContainer;
+    protected final Runner runner;
+
+    public FCFSScheduler(BlockingQueue<Task> taskArrivalQueue)
+    super(taskArrivalQueue);
+    fcfsTasksContainer = new FCFSTasksContainer();
     
 }
--- src/schedules/fcfsScheduling/FCFSTasksContainer.java
+++ src/schedules/fcfsScheduling/FCFSTasksContainer.java
@@ -1,5 +1,22 @@
 package schedules.fcfsScheduling;
 
-public class FCFSTasksContainer {
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.*;
+import schedules.SchedulingTasksContainer;
+import tasks.Task;
+
+public class FCFSTasksContainer implements SchedulingTasksContainer<Task> {
+    private final BlockingQueue<Task> TaskQueue = new LinkedBlockingQueue<>();
+
+    public void addTask(Task task) throws InterruptedException 
+    {
+        this.TaskQueue.put(task);
+    }
+
+    public boolean hasTasksAvailable()
+    {
+        return !TaskQueue.isEmpty();
+    }
     
 }
